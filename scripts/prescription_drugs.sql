970367fcc21ce36899b902b1e95f85057f2cd60d
Adam Crank <adam.crank@outlook.com>
2022-09-28 15:48:32 -0500

    Completed through number 7, 7a,b,c needs revision

--- scripts/prescription_drugs.sql
+++ scripts/prescription_drugs.sql
@@ -59,8 +59,73 @@ FROM drug;
 
 
 --4b)
-SELECT 
+SELECT SUM(total_drug_cost),
 	CASE WHEN opioid_drug_flag = 'Y' THEN 'opioid'
 		 WHEN antibiotic_drug_flag = 'Y' THEN 'antibiotic'
 		 ELSE 'neither' END AS drug_type
 FROM drug INNER JOIN prescription ON drug.drug_name = prescription.drug_name
+GROUP BY drug_type;
+
+
+--5a)
+SELECT COUNT(cbsa)
+FROM cbsa INNER JOIN fips_county ON cbsa.fipscounty = fips_county.fipscounty
+WHERE state = 'TN';
+
+
+--5b)
+SELECT cbsaname, SUM(population)
+FROM population INNER JOIN cbsa ON population.fipscounty = cbsa.fipscounty
+GROUP BY cbsaname
+ORDER BY SUM(population) DESC;
+
+
+--5c)
+SELECT county, SUM(population)
+FROM (SELECT fipscounty
+FROM population 
+EXCEPT
+SELECT fipscounty
+FROM cbsa) AS nocbsa INNER JOIN fips_county ON nocbsa.fipscounty = fips_county.fipscounty
+INNER JOIN population ON nocbsa.fipscounty = population.fipscounty
+GROUP BY county
+ORDER BY SUM(population) DESC;
+
+
+--6a)
+SELECT drug_name, total_claim_count
+FROM prescription
+WHERE total_claim_count >= 3000;
+
+
+--6b)
+SELECT prescription.drug_name, total_claim_count, opioid_drug_flag
+FROM prescription INNER JOIN drug ON prescription.drug_name = drug.drug_name
+WHERE total_claim_count >= 3000;
+
+--6c)
+SELECT prescription.drug_name, total_claim_count, opioid_drug_flag,
+	nppes_provider_first_name, nppes_provider_last_org_name
+FROM prescription INNER JOIN drug ON prescription.drug_name = drug.drug_name
+	INNER JOIN prescriber ON prescription.npi = prescriber.npi
+WHERE total_claim_count >= 3000;
+
+
+--7a) replace with crossjoin?
+SELECT prescriber.npi, /*nppes_provider_last_org_name, nppes_provider_first_name,
+	nppes_provider_city, specialty_description,*/ drug.drug_name
+FROM prescriber INNER JOIN prescription ON prescriber.npi = prescription.npi
+	INNER JOIN drug ON prescription.drug_name = drug.drug_name
+WHERE (specialty_description = 'Pain Management') 
+	AND (nppes_provider_city = 'NASHVILLE') 
+	AND (opioid_drug_flag = 'Y')
+ORDER BY npi DESC;
+
+
+--7b)
+SELECT npi, drug_name, SUM(total_claim_count) AS claims_per_drug
+FROM prescription
+GROUP BY npi, drug_name
+
+
+--7c)
